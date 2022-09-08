a8eaa1a87accc6f4d6a8bbc5b95537948c7852d4
igorfiquene <igorfpessoa@gmail.com>
2022-09-07 23:32:22 -0300

    feat: responsive grid show products

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,20 @@
+import { device } from './../../styles/global';
 import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const ProductList = styled.ul`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 20px;
   list-style: none;
+
+  @media ${device.mobileL} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media ${device.tablet} {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
   li {
     display: flex;
